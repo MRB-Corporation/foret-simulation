efeec0b1bc103713b9821440afa1d81e05aa1af0
Muhammad Redha Boutmene <mr.boutemene@gmail.com>
2026-05-26 07:08:41 +0200

    feat: ajout de l'interface AlgoPropa patern

--- src/main/java/simulation/algorithme/AlgorithmePropagation.java
+++ src/main/java/simulation/algorithme/AlgorithmePropagation.java
@@ -1,3 +1,15 @@
 package simulation.algorithme;
 
+import simulation.modele.Cellule;
+import simulation.modele.Grille;
+import java.util.List;
+
+public interface AlgorithmePropagation {
+
+    // Retourne la liste des voisins concernés par la propagation
+    List<Cellule> calculerVoisins(Cellule cellule, Grille grille);
+
+    // Nom de l'algo affiché dans l'interface graphique
+    String getNom();
+}
 // TODO
